6f528d94de33bc01bad2934e8d030a57fbe67ed2
Fernando Valelongo <fernandorocha16@hotmail.com>
2026-02-10 14:48:27 -0300

    F005 Tabela com apostadores e visualização de ranking dos apostadores  - Criação da pagina "RankingIndexViewModel.cs";  - Criação da pagina "RankingController.cs";  - Criação dentro de "Views" da pasta ranking e de seu index;  - Criação do botão para visualização do ranking dentro da pagina index;   OBS: Para visualização do ranking o usuário necessita estar logado.        Novo posicionamento para o botão de "admin"                      Comandos para o usuário visualizar confrontos ativados agora apenas pós login.                      Alterado css dos botões da tela inicial para uma melhor sinergia com o restante do site.

---
+++ BolaoDaCopa2026/ViewModels/RankingIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BolaoDaCopa2026.ViewModels
+{
+    public class RankingIndexViewModel
+    {
+        public DateTime AtualizadoEmUtc { get; set; }
+        public string? TotalPremioTexto { get; set; } // placeholder por enquanto
+
+        public List<RankingLinhaViewModel> Linhas { get; set; } = new();
+    }
+
+    public class RankingLinhaViewModel
+    {
+        public int Posicao { get; set; }
+        public string Nome { get; set; } = "";
+        public int Pontos { get; set; }
+
+        // NOVOS CAMPOS:
+        public int DistanciaDoPrimeiro { get; set; }     
+        public int PlacaresExatos { get; set; }          
+    }
+}
