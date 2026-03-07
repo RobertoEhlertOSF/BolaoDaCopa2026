3f6922f5714698fece5d26c332861313186fc705
Roberto Alvite Ehlert <63211875+RobertoEhlertOSF@users.noreply.github.com>
2026-03-07 17:42:52 -0300

    Melhorias Layout

---
+++ BolaoDaCopa2026/ViewModels/JogoComApostaViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BolaoDaCopa2026.Models.ViewModels
+{
+    public class JogoComApostaViewModel
+    {
+        public Jogo Jogo { get; set; }
+
+        public Aposta? ApostaUsuario { get; set; }
+    }
+}
